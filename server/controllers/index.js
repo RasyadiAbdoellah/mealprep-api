1889a6341a0b05bbc6edf8ffb65f4c10d3642a04
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-04-15 17:19:13 -0400

    add auth, fix linter, and format via prettier

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,15 +7,17 @@ const controllers = {}
 //dynamic require  of all files in directory
 //filters out hidden files and current file, then puts filename:require('./filename') key:val pari in to controllers object
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== filename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    if(file.slice(-3) === '.js'){
-      const name = file.slice(0, -3)
-      const fileDir = './' + name
-      controllers[name] = require(fileDir)
-    }
-  })
- 
+	.filter(file => {
+		return (
+			file.indexOf('.') !== 0 && file !== filename && file.slice(-3) === '.js'
+		)
+	})
+	.forEach(file => {
+		if (file.slice(-3) === '.js') {
+			const name = file.slice(0, -3)
+			const fileDir = './' + name
+			controllers[name] = require(fileDir)
+		}
+	})
+
 module.exports = controllers
--- server/models/RecipeIngredients.js
+++ server/models/RecipeIngredients.js
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-	const RecipeIngredients = sequelize.define('RecipeIngredients', {
-		RecipeId: DataTypes.INTEGER,
-		IngredientId: DataTypes.INTEGER,
-		val: DataTypes.INTEGER,
-		scale: DataTypes.STRING
-	}, {})
+	const RecipeIngredients = sequelize.define(
+		'RecipeIngredients',
+		{
+			RecipeId: DataTypes.INTEGER,
+			IngredientId: DataTypes.INTEGER,
+			val: DataTypes.INTEGER,
+			scale: DataTypes.STRING,
+		},
+		{},
+	)
 	RecipeIngredients.associate = function(models) {
 		// associations can be defined here
 	}
